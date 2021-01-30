9fa2a4c41d2ab3d56e5f247f01c5687f3108324b
Takuo OGAWA <oretakuan@gmail.com>
2021-01-30 22:07:32 +0900

    Add eslint, prettier and VSCode settings.json

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 function hello(name: string): string {
-    return `Hello, ${name}!`;
-  }
-  
-  console.log(hello("Hello World!!"));
+  return `Hello, ${name}!`;
+}
+
+console.log(hello('Hello World!!'));
